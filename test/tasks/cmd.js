518966777d9b6f0624d345959745d57939282f3d
Sharat M R <cosmosgenius@gmail.com>
2014-05-30 23:41:58 +0530

    added more test case

--- test/tasks/cmd.js
+++ test/tasks/cmd.js
@@ -1,10 +1,11 @@
 /*jslint node: true */
 /*jshint expr: true*/
-/*global describe, it*/
+/*global describe, it, before*/
 'use strict';
 
 var cmd     = require('../../app/tasks/cmd'),
-    should  = require('should');
+    should  = require('should'),
+    Q       = require('q');
 
 describe('cmd', function() {
     it('should exist', function(){
@@ -21,6 +22,31 @@ describe('cmd', function() {
 
     it('should throw type error if parameter is object or function', function(){
         cmd.bind(null,{}).should.throw(TypeError);
+    });
+
+    it('should throw type error if parameter function', function(){
         cmd.bind(null,function(){}).should.throw(TypeError);
+    });
+});
+
+describe('cmd ls', function() {
+    var ls;
+    before(function(){
+        ls = cmd('ls');
+    });
+
+    it('should return a promise', function(){
+        var promise = ls();
+        should.exist(promise);
+        should.exist(promise.then);
+        promise.then.should.be.a.Function;
+    });
+
+    it('should throw type error if parameter is object', function(){
+        ls.bind(null,{}).should.throw(TypeError);
+    });
+
+    it('should throw type error if parameter is function', function(){
+        ls.bind(null,function(){}).should.throw(TypeError);
     });
 });
