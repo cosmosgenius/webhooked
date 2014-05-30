a4be812b9389f9e41dbf3d32b46a18d6fdfb7c11
Sharat M R <cosmosgenius@gmail.com>
2014-05-30 21:28:31 +0530

    added tests for error case

--- test/tasks/cmd.js
+++ test/tasks/cmd.js
@@ -7,15 +7,20 @@ var cmd     = require('../../app/tasks/cmd'),
     should  = require('should');
 
 describe('cmd', function() {
-    it('cmd should exist', function(){
+    it('should exist', function(){
         should.exist(cmd);
     });
 
-    it('cmd should return a function', function(){
+    it('should return a function', function(){
         cmd('grunt').should.be.a.Function;
     });
 
-    it('cmd should throw type error if parameter not string', function(){
+    it('should throw type error if parameter is null', function(){
+        cmd.bind(null).should.throw(TypeError);
+    });
 
+    it('should throw type error if parameter is object or function', function(){
+        cmd.bind(null,{}).should.throw(TypeError);
+        cmd.bind(null,function(){}).should.throw(TypeError);
     });
 });
