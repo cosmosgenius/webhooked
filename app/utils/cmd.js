672e6df5c2770cac142c8dca041529505bf4cef7
Sharat M R <cosmosgenius@gmail.com>
2014-05-31 00:30:12 +0530

    renamed folder to utils

---
+++ app/utils/cmd.js
@@ -0,0 +1,36 @@
+/*jslint node: true */
+'use strict';
+
+var exec = require('child_process').exec,
+    Q = require('q');
+
+module.exports = function(command) {
+
+    if (typeof command !== 'string') {
+        throw new TypeError('command should be of type string');
+    }
+
+    return function(path) {
+
+        if (!path) {
+            path = '.';
+        }
+
+        if (typeof path !== 'string') {
+            throw new TypeError('command should be of type string');
+        }
+
+        return Q.Promise(function(resolve, reject) {
+            exec(command,{
+                cwd : path
+            }, function(error, stdout) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(stdout);
+                }                
+            });
+
+        });
+    };
+};
