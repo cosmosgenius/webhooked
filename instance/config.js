9196096bef997cda421375f33c6a37fc22161644
Sharat M R <cosmosgenius@gmail.com>
2014-06-02 03:07:16 +0530

    added db configs

--- instance/config.js
+++ instance/config.js
@@ -4,6 +4,11 @@ var config      = {};
 
 config.port     = 8000;
 config.env      = process.env.NODE_ENV || 'development';
+config.db       = 'whkd';
+config.dbuser   = 'whkd';
+config.dbpass   = 'whkd';
+config.dbtype   = 'postgres';
+config.dbport   = 5432;
 
 if (config.env  === 'test') {
     config.port     = 8500;
