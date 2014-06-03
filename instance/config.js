9ae26e850e6d1a138ad71536d601ab8485b4bb28
Sharat M R <cosmosgenius@gmail.com>
2014-06-04 04:06:26 +0530

    updated config for db

--- instance/config.js
+++ instance/config.js
@@ -5,16 +5,14 @@ var config      = {};
 config.port     = 8000;
 config.env      = process.env.NODE_ENV || 'development';
 config.db       = 'whkd';
-config.dbuser   = 'whkd';
-config.dbpass   = 'whkd';
+config.dbuser   = 'postgres';
+config.dbpass   = null;
 config.dbtype   = 'postgres';
 config.dbport   = 5432;
 
 if (config.env  === 'test') {
     config.port     = 8500;
     config.db       = 'whkd_test';
-    config.dbuser   = 'whkd_test';
-    config.dbpass   = 'whkd_test';
 }
 
 module.exports  = config;
