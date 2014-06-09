d33113a2ee5103e33f73436fd912860f3ad4d714
Sharat M R <cosmosgenius@gmail.com>
2014-06-10 03:54:18 +0530

    updated tests

--- test/models/app.js
+++ test/models/app.js
@@ -3,15 +3,13 @@
 /*global describe, it, before*/
 'use strict';
 
-var App     = require('../../app/models/app'),
-    should  = require('should');
+var App = require('../../app/models').App,
+    should = require('should');
 
 describe('App', function() {
     describe('Object', function() {
         before(function(done) {
-            App.sync({
-                force: true
-            }).done(done);
+            done();
         });
 
         it('should exist', function() {
