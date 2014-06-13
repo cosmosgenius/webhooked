2f2d09d894358fe3af376f42aecec9fdc15a4fe7
Sharat M R <cosmosgenius@gmail.com>
2014-06-14 00:51:26 +0530

    added more verbs to webapps

--- app/routes/webapps.js
+++ app/routes/webapps.js
@@ -3,14 +3,26 @@
 
 var express = require('express'),
     webapps = express.Router(),
-    AppModel = require('../models/app');
+    AppModel = require('../models').App;
 
 module.exports = webapps;
 
-webapps.get('/',function(req, res){
-    res.send('hello');
-});
+webapps.route('/')
+    .get(function(req, res) {
+        res.send('hello');
+    })
+    .post(function(req, res) {
+        res.send('Post hello');
+    });
 
-webapps.get('/:app',function(req, res){
-    res.send(req.params);
-});
+
+webapps.route('/:app')
+    .get(function(req, res) {
+        res.send(req.params);
+    })
+    .post(function(req, res) {
+        res.send(req.params);
+    })
+    .delete(function(req, res) {
+        res.send(req.params);
+    });
