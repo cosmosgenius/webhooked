3140640fa2bb0355a8ced429cc2559c2b2d19376
Sharat M R <cosmosgenius@gmail.com>
2014-06-14 23:06:22 +0530

    removed seq sync

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ var http    = require('http'),
     config  = require('config'),
     app     = require('./app'),
     server  = http.createServer(app);
-    
-var db      = app.get('db');
 
-db.sync().complete(function() {
-    server.listen(config.port, function(err) {
-        if (err) {
-            console.log(err);
-        }
-        console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-    });
+server.listen(config.port, function(err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
