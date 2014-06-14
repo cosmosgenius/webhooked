abcb7bdaa782f7d2bf57c5c81ec5dbf26d4210a6
Sharat M R <cosmosgenius@gmail.com>
2014-06-14 23:07:35 +0530

    some updates on deploy api

--- app/routes/deploy.js
+++ app/routes/deploy.js
@@ -2,9 +2,14 @@
 'use strict';
 
 var express = require('express'),
-    deploy = express.Router();
+    deploy  = express.Router();
 
 module.exports = deploy;
+
+deploy.param('app', function(req, res, next, name){
+    req.appname = name;
+    next();
+});
 
 deploy.route('/')
     .get(function(req, res) {
@@ -13,8 +18,8 @@ deploy.route('/')
 
 deploy.route('/:app')
     .get(function(req, res) {
-        res.send(req.params);
+        res.send(req.appname);
     })
     .post(function(req, res) {
-        res.send(req.params);
+        res.send(req.appname);
     });
