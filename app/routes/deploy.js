c2cb749b4d00e9fe7c27f999c8265a1bfbfd2e21
Sharat M R <cosmosgenius@gmail.com>
2014-06-16 02:00:00 +0530

    update deploy to execute commands

--- app/routes/deploy.js
+++ app/routes/deploy.js
@@ -2,14 +2,18 @@
 'use strict';
 
 var express = require('express'),
-    deploy  = express.Router(),
-    db      = require('../models'),
-    App     = db.App;
+    Q = require('q'),
+    deploy = express.Router(),
+    path = require('../utils/path'),
+    db = require('../models'),
+    App = db.App;
 
 module.exports = deploy;
 
-deploy.param('app', function(req, res, next, name){
-    App.findOne({name : name}, function(err, app){
+deploy.param('app', function(req, res, next, name) {
+    App.findOne({
+        name: name
+    }, function(err, app) {
         req.app = app;
         next();
     });
@@ -17,15 +21,41 @@ deploy.param('app', function(req, res, next, name){
 
 deploy.route('/')
     .get(function(req, res) {
-        App.find(function(err, app){
+        App.find(function(err, app) {
             res.json(app);
         });
     });
 
 deploy.route('/:app')
     .get(function(req, res) {
-        if(!req.app){
-            return res.json(404, {error: 'App doesn\'t exist'});
+        if (!req.app) {
+            return res.json(404, {
+                error: 'App doesn\'t exist'
+            });
         }
-        res.json(req.app);
+
+        var execute = path(req.app.path);
+        var tasks = req.app.tasks;
+        var taskPromise = [];
+
+        tasks.forEach(function(task) {
+            taskPromise.push(execute(task));
+        });
+
+        Q.all(taskPromise).then(function(results) {
+            var response = '';
+            
+            results.forEach(function(result) {
+                response += result;
+            });
+
+            res.json({result:response});
+
+        }).fail(function(err) {
+
+            res.json(400, {
+                error: err.message
+            });
+
+        });
     });
