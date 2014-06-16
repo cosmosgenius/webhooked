bc31607ae2ad15aa53bb4998d01f6428735f2b3d
Sharat M R <cosmosgenius@gmail.com>
2014-06-17 00:52:35 +0530

    added handle for 404 and delete

--- app/routes/webapps.js
+++ app/routes/webapps.js
@@ -11,7 +11,9 @@ module.exports = webapps;
 
 webapps.route('/')
     .get(function(req, res) {
-        res.send('hello');
+        App.find(function(err, app) {
+            res.json(app);
+        });
     })
     .post(function(req, res, next) {
         var data = '';
@@ -25,7 +27,7 @@ webapps.route('/')
         });
     })
     .post(function(req, res) {
-        var JSONobj,newApp;
+        var JSONobj, newApp;
         if (!req.is('json')) {
             return res.json(400, {
                 error: 'Type should be json'
@@ -57,14 +59,36 @@ webapps.route('/')
         });
     });
 
+webapps.param('app', function(req, res, next, name) {
+    App.findOne({
+        name: name
+    }, function(err, app) {
+        req.app = app;
+        next();
+    });
+});
 
 webapps.route('/:app')
-    .get(function(req, res) {
-        res.send(req.params);
+    .all(function(req, res, next) {
+        if (!req.app) {
+            return res.json(404, {
+                error: 'App doesn\'t exist'
+            });
+        }
+        next();
     })
-    .post(function(req, res) {
-        res.send(req.params);
+    .get(function(req, res) {
+        return res.json(req.app);
+    })
+    .put(function(req, res) {
+        res.send(req.app);
     })
     .delete(function(req, res) {
-        res.send(req.params);
+        req.app.remove(function(err) {
+            if (err) {
+                res.json(err);
+            }
+            return res.send(204);
+        });
+
     });
