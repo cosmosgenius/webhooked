c8056c48417c5c20b52f4fb88d8b8f934df6046a
Sharat M R <cosmosgenius@gmail.com>
2014-08-26 02:35:00 +0530

    added some documentation and moduled into functions

--- app/utils/path.js
+++ app/utils/path.js
@@ -1,26 +1,27 @@
-/*jslint node: true */
 'use strict';
 
 var exec = require('child_process').exec,
     Q = require('q');
 
-module.exports = function(path) {
+/**
+ * Map to handle messages
+ * @constant
+ * @type {Object}
+ */
+var messages = {
+    pathError : 'path should be of type string',
+    commandError : 'command should be of type string'
+};
 
-    if (!path) {
-        path = '.';
-    }
-
-    if (typeof path !== 'string') {
-        throw new TypeError('path should be of type string');
-    }
-
-    return function(command) {
-
-        if (typeof command !== 'string') {
-            throw new TypeError('command should be of type string');
-        }
-
-        return Q.Promise(function(resolve, reject) {
+/**
+ * Executes the given command at the given path
+ * @param  {String} command 
+ * @param  {String} path    
+ * @return {Promise}
+ * @see executeCommand
+ */
+function executeCommand(command, path){
+    return Q.Promise(function(resolve, reject) {
             exec(command, {
                 cwd: path
             }, function(error, stdout) {
@@ -32,5 +33,29 @@ module.exports = function(path) {
             });
 
         });
+}
+
+/**
+ * A generator which returns a function which accepts command to be executed at the given path
+ * @method generatePath
+ * @param  {String} path
+ * @return {Function}     a function which accepts a command as parameter
+ */
+function generatePath(path) {
+    if (!path) {
+        path = '.';
+    }
+
+    if (typeof path !== 'string') {
+        throw new TypeError(messages.pathError);
+    }
+
+    return function(command) {
+        if (typeof command !== 'string') {
+            throw new TypeError(messages.commandError);
+        }
+        return executeCommand(command, path);
     };
-};
+}
+
+module.exports = generatePath;
