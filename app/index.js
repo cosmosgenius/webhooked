bf3d47aefcac55d0ef3f76f610b097bb968d805f
Sharat M R <cosmosgenius@gmail.com>
2014-09-12 03:38:16 +0530

    added cors

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
 "use strict";
 
 var express         = require("express"),
+    cors            = require("cors"),
     app             = express();
 
 var webapps         = require("./routes/webapps"),
     deploy          = require("./routes/deploy");
     
 
-
+app.use(cors());
 app.use("/webapps",webapps);
 app.use("/deploy",deploy);
 
