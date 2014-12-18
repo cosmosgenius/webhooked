aea672cd3f636104aefded991a632f99dd3c7921
Sharat M R <cosmosgenius@gmail.com>
2014-12-19 05:03:38 +0530

    Deleted old models

--- app/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var config      = require("config");
-var manager     = require("./mongoose.manager");
-
-manager.connect(config.mongouri);
-module.exports = manager;
-module.exports.App = manager.createModel("App",require("./app.model"));
