dabc48fd114c1d1d074ad93f5a01bfb40efb2b2d
Sharat M R <cosmosgenius@gmail.com>
2016-05-07 04:57:39 +0530

    initial route

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 'use strict';
 
-const express = require('express');
+const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const co = require('co');
 
-let app = express();
+let app =  new Koa();
+
+let route = new KoaRouter();
+
+route.get('/', co.wrap(function* (ctx, next) {
+    ctx.body = 'Hello World!';
+    yield next();
+}));
+
+app
+    .use(route.routes())
+    .use(route.allowedMethods());
 
 module.exports = app;
