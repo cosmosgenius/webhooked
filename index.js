012b4c473f90f67e04f8d0f203c3cf17f8f87605
Sharat M R <cosmosgenius@gmail.com>
2016-05-07 18:48:12 +0530

    add server listen logs and made bunyan output short

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 require('dotenv').config();
+require('./logging');
+
+const http = require('http');
 const app = require('./server');
 
 let port = process.env.PORT || 6000;
 
-app.listen(port);
+const server = http.createServer(app.callback());
+
+server.listen(port, () => {
+    const address = server.address();
+    console.log(`Listen to ${address.address} on port ${address.port} in ${app.env} env`);
+});
