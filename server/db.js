707fc5c8d68081b0b644bccdf7b64e42ae55ac41
Sharat M R <cosmosgenius@gmail.com>
2016-05-08 03:40:23 +0530

    add a store class and using that in model

--- server/db.js
+++ server/db.js
@@ -1,50 +1,67 @@
 'use strict';
 const level = require('level');
 
-let db = level(process.env.DB_PATH || './db', {
-    'valueEncoding': 'json'
-});
+class Store {
+    constructor() {
+        this.db = level(process.env.DB_PATH || './db', {
+            'valueEncoding': 'json'
+        });
+    }
+
+    put (key, value) {
+        return new Promise((resolve, reject) => {
+            this.db.put(key, value, (err) => {
+                if (err) return reject(err);
+                resolve(value);
+            });
+        });
+    }
+
+    get (key) {
+        return new Promise((resolve, reject) => {
+            this.db.get(key, (err, value) => {
+                if(err) return reject(err);
+                resolve(value);
+            });
+        });
+    }
+}
+
+let db = new Store;
 
 class Model {
     constructor (id) {
         this.id = id;
     }
 
+    validate () {}
+
     save() {
-        return new Promise((resolve, reject) => {
-            if(this.validate) this.validate();
-            let key = this.getKey();
-            let val = {};
+        this.validate();
+        const key = this.getKey();
+        let values = {};
 
-            for(let vkey in this) {
-                val[vkey] = this[vkey];
-            }
-
-            db.put(key, val, (err)=> {
-                if(err) return reject(err);
-                resolve(this);
-            });
-        });
+        for(let vkey in this) {
+            values[vkey] = this[vkey];
+        }
+        return db.put(key, values);
     }
 
     getKey() {
         return `${this.constructor.name.toLowerCase()}:${this.id}`;
     }
 
-    static findById(id) {
+    static findById (id) {
         let key = `${this.name.toLowerCase()}:${id}`;
-        return new Promise((resolve, reject) => {
-            db.get(key, (err, obj) => {
-                if(err) return reject(err);
 
-                let vobj = new this(obj.id);
-
-                for(let vkey in obj) {
-                    vobj[vkey] = obj[vkey];
-                }
-                resolve(vobj);
-            });
-        });
+        return db.get(key)
+                .then((obj)=>{
+                    let vobj = new this(obj.id);
+                    for(let vkey in obj) {
+                        vobj[vkey] = obj[vkey];
+                    }
+                    return vobj;
+                });
     }
 }
 
