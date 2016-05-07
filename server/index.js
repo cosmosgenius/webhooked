2659b48739b7d087feec7685445788306c97b7be
Sharat M R <cosmosgenius@gmail.com>
2016-05-07 19:47:31 +0530

    add koa response time

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
+const responseTime = require('koa-response-time');
 const co = require('co');
 
 let app =  new Koa();
@@ -14,6 +15,7 @@ route.get('/', co.wrap(function* (ctx, next) {
 }));
 
 app
+    .use(responseTime())
     .use(route.routes())
     .use(route.allowedMethods());
 
