e8f07d5bdd2df69d24268e4739c378b37cc0abf6
Sharat M R <cosmosgenius@gmail.com>
2016-05-12 06:18:38 +0530

    refactor error

--- server/error.js
+++ server/error.js
@@ -6,13 +6,16 @@ function* errorHandler(ctx, next) {
         yield next();
     } catch (err) {
         ctx.status = err.statusCode || err.status || 500;
-        if(!(err.message instanceof Array)) {
-            err.message = [{
-                message: err.message
+
+        let messages = err.message;
+
+        if(!(messages instanceof Array)) {
+            messages = [{
+                message: messages
             }];
         }
         ctx.body = {
-            errors: err.message
+            errors: messages
         };
 
         if(ctx.status >= 500) {
