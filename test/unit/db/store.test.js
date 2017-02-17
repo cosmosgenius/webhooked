32a60ec4b773752c21ae6bab3a4ad91dbdbaf0cd
Sharat M R <cosmosgenius@gmail.com>
2017-02-17 16:05:56 +0530

    Move test to async await and create a test-db

--- test/unit/db/store.test.js
+++ test/unit/db/store.test.js
@@ -2,36 +2,30 @@ const store = require('../../../server/db/store');
 const db = store.db;
 
 describe('Store', () => {
-    it('put', (done) => {
-        store.put('testput', 123).then(()=>{
-            db.get('testput', (err, value) => {
-                expect(value).toEqual(123);
-                done(err);
-            });
-        });
+    it('get', async () => {
+        try {
+            await store.get('testget');
+        } catch (err) {
+            expect(err.name).toEqual('NotFoundError');
+        }
+        await store.put('testget', 1234);
+        var value =  await store.get('testget');
+        expect(value).toEqual(1234);
     });
 
-    it('get', (done) => {
-        db.put('testget', 1234, (err) => {
-            if(err) done(err);
-            store.get('testget').then((value)=>{
-                expect(value).toEqual(1234);
-                done();
-            }).catch(done);
-        });
+    it('put', async () => {
+        await store.put('testput', 123);
+        var value =  await store.get('testput');
+        expect(value).toEqual(123);
     });
 
-    it('del', (done) => {
-        store.put('testdel', 2345)
-            .then(()=> {
-                return store.del('testdel');
-            })
-            .then(()=> {
-                db.get('testdel', (err, value) => {
-                    expect(err.name).toEqual('NotFoundError');
-                    expect(value).toBeUndefined();
-                    done();
-                });
-            }).catch(done);
+    it('del', async () => {
+        await store.put('testdel', 2345)
+        await store.del('testdel');
+        try {
+            await store.get('testdel');
+        } catch (err) {
+            expect(err.name).toEqual('NotFoundError');
+        }
     });
 });
