cff604e30e359b18af7ff9572a7c4712b2fb4d7d
Sharat M R <cosmosgenius@gmail.com>
2017-02-28 12:48:11 +0530

    Disable model test

--- test/unit/db/model.test.js
+++ test/unit/db/model.test.js
@@ -1,7 +1,3 @@
-const store = require('../../../lib/db/store');
-const Model = require('../../../lib/db/model');
-
 describe('Model', () => {
-    class MockModel extends Model{}
     it('test model');
 });
